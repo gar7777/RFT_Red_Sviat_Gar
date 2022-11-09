692893b798a92a3fdbb17d51eabdb2f4f029b3fb
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-11-09 13:39:53 +0300

    refactor: add sass, change structure

--- management-app/src/components/App/App.tsx
+++ management-app/src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../logo.svg';
+import styles from './App.module.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>

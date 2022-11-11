7716cb67d6ecc7f2a76d6245dfcac5161eb4b7bc
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-11-11 21:09:33 +0300

    feat: add header

--- management-app/src/components/Header/Header.tsx
+++ management-app/src/components/Header/Header.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
-import { Home } from '@mui/icons-material';
+import { AppBar, Button, Toolbar } from '@mui/material';
+import { Home, Language, Login, Logout } from '@mui/icons-material';
+import Menu from './Menu/Menu';
+import styles from './Header.module.scss';
+import BtnLogIn from './Menu/BtnLogIn';
+import BtnLogOut from './Menu/BtnLogOut';
+import BtnProfile from './Menu/BtnProfile';
+import BtnSignUp from './Menu/BtnSignUp';
 
 function Header() {
+  console.log(styles);
+  const isAuthorized = false;
   return (
-    <AppBar>
-      <Toolbar>
-        <IconButton>
-          <Home />
-        </IconButton>
-        <Typography>Главная</Typography>
+    <AppBar className={styles.appbar} position="sticky">
+      <Toolbar className={styles.toolbar}>
+        <div className={styles.toolbarItem}>
+          <Home fontSize="large" />
+          <Button variant="text" sx={{ color: 'white', fontSize: '1.3rem', fontWeight: '400' }}>
+            ГЛАВНАЯ
+          </Button>
+        </div>
+        <div className={styles.toolbarItem} style={{ marginLeft: 'auto' }}>
+          <Language fontSize="large" />
+          <Menu />
+        </div>
+        <div className={styles.toolbarItem}>{!isAuthorized ? <BtnLogIn /> : <BtnLogOut />}</div>
+        <div className={styles.toolbarItem}>{!isAuthorized ? <BtnSignUp /> : <BtnProfile />}</div>
       </Toolbar>
     </AppBar>
   );

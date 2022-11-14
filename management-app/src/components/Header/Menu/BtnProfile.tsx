31fee7d82c9c6de8e49658c4bd3f4e6a1f8bea77
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-11-14 16:44:08 +0300

    feat: add authSlice

--- management-app/src/components/Header/Menu/BtnProfile.tsx
+++ management-app/src/components/Header/Menu/BtnProfile.tsx
@@ -1,14 +1,17 @@
 import { AccountCircle } from '@mui/icons-material';
 import { Button } from '@mui/material';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function BtnProfile() {
   return (
     <>
       <AccountCircle fontSize="large" />
-      <Button variant="text" sx={{ color: 'white', fontSize: '1.3rem', fontWeight: '400' }}>
-        ПРОФИЛЬ
-      </Button>
+      <NavLink to="/profile">
+        <Button variant="text" sx={{ color: 'white', fontSize: '1.3rem', fontWeight: '400' }}>
+          ПРОФИЛЬ
+        </Button>
+      </NavLink>
     </>
   );
 }

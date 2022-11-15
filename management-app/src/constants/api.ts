d3f2e7d244c1a8322203b2e34501d503dc3375bb
Gar <gar@tut.by>
2022-11-15 21:04:23 +0300

    feat: add board route

--- management-app/src/constants/api.ts
+++ management-app/src/constants/api.ts
@@ -1,3 +1,3 @@
 export const API_URL = 'https://team2-manager-app.herokuapp.com';
 export const TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJlZWNiYzE5MC1lN2I2LTQ2MDUtODYyNi05MzlmNDkwYzQ3ZjQiLCJsb2dpbiI6InVzZXIwMDEiLCJpYXQiOjE2NjgzNjIxNzd9.qUufoTgcl_NRrjrv7trQLC6nimi1MWiZTO4DEW-iXwM'; // todo remove
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmMjVkM2Y5ZS01OWYwLTQzZjItOWM1NS03ZGEwMzIzNzA0ZGEiLCJsb2dpbiI6IlVzZXIzIiwiaWF0IjoxNjY4MjcyMjYzfQ.jmKAJHj2Pzy8hlXXVYId8aDapjhd9bt3jG32jNQCvi8'; // todo remove

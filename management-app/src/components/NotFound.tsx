6a0ddb53985408b1a233b94b4be07eb446f8cc51
MatsurSviat <hereyourare@rambler.ru>
2022-11-20 13:17:52 +0300

    feat: add NotFound page component

--- management-app/src/components/NotFound.tsx
+++ management-app/src/components/NotFound.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import errorPageStyle from './scss/ErrorPage.module.scss';
 
-function NotFound() {
-  return <div>NotFound</div>;
+export function NotFound() {
+  return (
+    <div className={errorPageStyle.error}>
+      <div className={errorPageStyle.error_message}>
+        <h1>404 ERROR</h1>
+        <p>This page doesn&apos;t exist</p>
+        <p>
+          Would you like to return to
+          <span>
+            <Link to="/boards" className={errorPageStyle.return}>
+              &nbsp;HOME page?&nbsp;
+            </Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 }
-
-export default NotFound;

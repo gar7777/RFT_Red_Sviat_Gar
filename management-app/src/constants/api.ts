1839357815a6e11cdcda8e937c4e3dd6916da464
Gar <gar@tut.by>
2022-12-04 22:16:37 +0300

    bild: first version

--- management-app/src/constants/api.ts
+++ management-app/src/constants/api.ts
@@ -1,3 +1,1 @@
-export const API_URL = 'http://localhost:4000';
-// export const API_URL = 'https://team2-manager-app.herokuapp.com';
-// export const API_URL = 'https://final-task-backend-production-adca.up.railway.app';
+export const API_URL = 'https://team2-manager-app.herokuapp.com';
